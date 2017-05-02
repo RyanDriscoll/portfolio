1b3458e2995a5f4678d2f4e3a6ddec2f58a9352b
Ryan Driscoll <driscollrp@gmail.com>
2017-05-02 10:08:24 -0500

    clipping for content areas

--- index.js
+++ index.js
@@ -7,11 +7,31 @@ $(document).ready(function() {
     $body = $('body'),
     $navHeader = $('.nav-header'),
     $bannerText = $('#banner-text'),
+    $logo = $('.logo'),
+    $slide = $('.slide'),
+    $about = $slide.children(':nth-child(1)'),
+    $projects = $slide.children(':nth-child(2)'),
+    $resume = $slide.children(':nth-child(3)'),
+    $contact = $slide.children(':nth-child(4)'),
     $window = $(window);
 
   let
     $selectedNav = $listItem.first(),
-    showNav = true;
+    viewWidth = $body.width(),
+    showNav = true,
+    enter,
+    leave;
+
+  // toggle navbar on logo hover
+  $logo.hover(function() {
+    if (!showNav) {
+      toggleNav(viewWidth);
+    }
+  }, function() {
+    if (!showNav) {
+      toggleNav(viewWidth);
+    }
+  });
 
   // set initial line position
   setTimeout(() => {
@@ -30,14 +50,16 @@ $(document).ready(function() {
 
   // move line on hover
   $listItem.hover(function(){
-    const $this = $(this);
-    setTimeout(function() {
+    let $this = $(this);
+    clearTimeout(leave);
+    enter = setTimeout(function() {
       moveLine($this.position().left, $this.width());
-    }, 500);
+    }, 300);
   }, function() {
-    setTimeout(function() {
+    clearTimeout(enter);
+    leave = setTimeout(function() {
       moveLine($selectedNav.position().left, $selectedNav.width());
-    }, 500);
+    }, 300);
   });
 
   // move line under nav options
@@ -49,29 +71,62 @@ $(document).ready(function() {
   }
 
   // toggle navbar
-  function toggleNav(top) {
+  function toggleNav(width) {
+    let operator = showNav ? '-' : '+';
+    $navHeader.animate({left: `${operator}=${width}px`});
+    showNav = !showNav;
+  }
+
+  // scroll element up at rate
+  function parallaxUp(element, rate) {
+    element.css({top: rate});
+  }
+
+  function showSlideText(element, offsetTop, offsetBottom) {
     let
-      operator = showNav ? '-' : '+';
-    function toggle(width) {
-      $navHeader.animate({left: `${operator}=${width}px`});
-      showNav = !showNav;
-    }
-    if ((showNav && top > 25) || (!showNav && top <= 25)) {
-      let viewWidth = $body.width();
-      toggle(viewWidth);
-    }
+      top = offsetTop,
+      bottom = offsetBottom;
+    element.css({
+      'clip-path': `inset(${top}px 0 ${bottom}px`
+    });
   }
 
-  // move element up
-  function parallaxUp(node, rate) {
-    node.css({top: rate});
-  }
-
-
+  // window scroll events
   $window.scroll(function() {
-    let top = $body.scrollTop();
-    toggleNav(top);
-    parallaxUp($bannerText, top * -1.15);
+    let
+      top = $body.scrollTop(),
+      windowHeight = window.innerHeight;
+    parallaxUp($bannerText, top * -1.35);
+
+    if ((showNav && top > 25) || (!showNav && top <= 25)) {
+      toggleNav(viewWidth);
+    }
+
+    // $slideContent.first().css({
+    //   'clip-path': `inset(${(600 - top) - 390}px 0 0)`
+    // });
+    console.log(600 - top, (600 + windowHeight)
+    + top);
+    showSlideText(
+      $about,
+      windowHeight - top,
+      (windowHeight * -1) + top
+    );
+    // showSlideText(
+    //   $projects,
+    //   (600 + windowHeight) - top,
+    //   (600 + (windowHeight * 2)) - top
+    // );
+    // showSlideText(
+    //   $resume,
+    //   (600 + (windowHeight * 2)) - top,
+    //   (600 + (windowHeight * 3)) - top
+    // );
+    // showSlideText(
+    //   $contact,
+    //   (600 + (windowHeight * 3)) - top,
+    //   (600 + (windowHeight * 4)) - top
+    // );
   });
 
 });
